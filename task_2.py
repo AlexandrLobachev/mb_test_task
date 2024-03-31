4d539c3f8017294cc810848ef498ae62b9020f8d
AlexandrLobachev <alexandrlobachev@gmail.com>
2024-03-31 21:53:54 +0300

    v1.1

--- task_2.py
+++ task_2.py
@@ -6,10 +6,11 @@ def get_products(products_df, categories_df, relations_df):
         products_df, on="product_id"
     ).join(categories_df, on="category_id"
     ).select(
-        concat("product_name", lit(' - '),"category_name").alias('Results'))
+        concat("product_name", lit(' - '),"category_name").alias('Results')
+    ).orderBy("product_name")
 
     products_wo_cat = products_df.join(
         relations_df, on="product_id", how="left_anti"
-    ).select("product_name")
+    ).select("product_name").orderBy("product_name")
 
     return products_w_cat.union(products_wo_cat)
